fb9e1bd76142b50c2e47546c56f07211e1235a6c
BernardinoR <rafaelbsilveiira@gmail.com>
2025-04-17 14:56:02 -0300

    etapa 2

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        screen.fill(constants.BLACK)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
